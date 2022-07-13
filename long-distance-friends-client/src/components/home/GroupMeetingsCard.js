42331283092c3b302281c0756a8d323697af0c80
jcyling <jane.cy.ling@gmail.com>
2022-07-13 13:03:23 +0800

    Update meeting card to show RSVP link

--- long-distance-friends-client/src/components/home/GroupMeetingsCard.js
+++ long-distance-friends-client/src/components/home/GroupMeetingsCard.js
@@ -3,11 +3,13 @@ import { convertUtcToDateRange } from "../common/TimeUtils";
 
 const GroupMeetingsCard = ({ meeting, user, handleHangoutDelete }) => {
 
+  const invitationUrl = `localhost:3000/rsvp/${meeting.uid}`;
   const UtcWindow = convertUtcToDateRange(meeting.window, user.timezone);
 
   return (
     <div className="p-3 rounded-md text-left border">
       <div className="flex flex-col">
+
         <div>
           {UtcWindow.startDate} To {UtcWindow.endDate}
         </div>
@@ -15,14 +17,17 @@ const GroupMeetingsCard = ({ meeting, user, handleHangoutDelete }) => {
           Edit your availability
         </div>
       </div>
+      <div>
+        Send your friends this link to RSVP:
+        <div>
+          <a href={invitationUrl}>{invitationUrl}</a>
+        </div>
+      </div>
       <button
-        className="float-right text-gray-500"
+        className="text-gray-500"
         onClick={() => handleHangoutDelete(meeting.id)}>
         Cancel Hangout
       </button>
-      <div>
-        Send your friends this link to RSVP
-      </div>
     </div>
   );
 };
