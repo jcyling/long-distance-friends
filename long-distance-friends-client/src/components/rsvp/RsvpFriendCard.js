979c791a990954a0e62330f63a8e64dbc0114419
jcyling <jane.cy.ling@gmail.com>
2022-08-02 15:19:42 +0100

    Minor styling edits to Friend components

--- long-distance-friends-client/src/components/rsvp/RsvpFriendCard.js
+++ long-distance-friends-client/src/components/rsvp/RsvpFriendCard.js
@@ -20,8 +20,8 @@ const RsvpFriendCard = ({ booked, friend, handleFriendPick, activeFriend }) => {
   if (disabled) {
     return (
       <div
-        className="m-6 flex flex-col items-center">
-        <div className={"bg-gray-500 w-24 h-24 rounded-full"}></div>
+        className="bg-gradient-to-r from-cyan-500 to-blue-500 m-6 flex flex-col items-center">
+        <div className={" w-24 h-24 rounded-full"}></div>
         <div>{friend.name}</div>
         <div className="flex flex-col text-sm">
           <span>{friend.timezone}</span>
@@ -35,9 +35,10 @@ const RsvpFriendCard = ({ booked, friend, handleFriendPick, activeFriend }) => {
       <div
         className="m-6 flex flex-col items-center cursor-pointer"
         onClick={() => handleFriendPick(friend)}>
-        <div className={`bg-gray-300 w-24 h-24 rounded-full ${active ? "border-2 border-blue-500" : ""}`}></div>
-        <div>{friend.name}</div>
-        <span className="text-sm">{friend.timezone}</span>
+        <div className={`flex flex-col justify-center bg-gradient-to-r from-blue-200 to-gray-200 w-24 h-24 rounded-full ${active ? "border-2 border-blue-500" : ""}`}>
+          <div className="text-gray-700 text-lg">{friend.name}</div>
+        </div>
+        <span className="text-sm mt-2">{friend.timezone}</span>
       </div>
     );
   }
